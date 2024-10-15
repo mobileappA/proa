137d960f0ffeed5c73017a95af91c469ca485ad6
mobileappA <appm81554@gmail.com>
2024-10-16 00:38:54 +0700

    22

--- r-checklogin.php
+++ r-checklogin.php
@@ -2,7 +2,7 @@
 session_start();
 if (!isset($_SESSION['aid'])) {
   echo '<p style="color:red;">' . htmlspecialchars('Access Denied!', ENT_QUOTES, 'UTF-8') . '</p>';
-  header('Refresh: 3; URL=../pro/r-login.php');
+  header('Refresh: 3; URL=../proa/r-login.php');
   exit;
 }
 ?>
