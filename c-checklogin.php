de8424e46354a56fcab9cbdfbf6b6038264640e1
mobileappA <appm81554@gmail.com>
2024-10-17 11:34:47 +0700

    AA

--- c-checklogin.php
+++ c-checklogin.php
@@ -3,7 +3,7 @@
 session_start();
 if (!isset($_SESSION['cid'])) {
   echo '<p style="color:red;">' . htmlspecialchars('Access Denied!', ENT_QUOTES, 'UTF-8') . '</p>';
-  header('Refresh: 3; URL=../pro/index.php');
+  header('Refresh: 3; URL=../proa/index.php');
   exit;
 }
 ?>
